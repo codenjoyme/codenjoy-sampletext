113c7400ab442717e27a21393969dea1c7d89297
Oleksandr_Baglai <apofig@gmail.com>
2021-02-23 14:47:51 +0200

    [games] Refactoring: Renname main game logic test (SampleTest -> SingleplayerTest, SingleTest -> MultiplayerTest)

--- src/test/java/com/codenjoy/dojo/sampletext/model/GameTest.java
+++ src/test/java/com/codenjoy/dojo/sampletext/model/GameTest.java
@@ -37,7 +37,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class SingleplayerTest {
+public class GameTest {
 
     private SampleText game;
     private Hero hero;
